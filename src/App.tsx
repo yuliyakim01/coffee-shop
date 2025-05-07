80fba2869829a831b378167457541dfb324ff56b
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-05-07 12:51:55 +0400

    feat: add eslint prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello React with TypeScript!</h1>
+      <p>Your React app is now set up with Webpack and TypeScript.</p>
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,72 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.ts", // Change to .ts or .tsx if using React
+  entry: './src/index.tsx',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
-  mode: "development",
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    port: 3000,
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: "ts-loader",
+        test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            plugins: [['@babel/plugin-transform-runtime', { regenerator: true }]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
-  devtool: "inline-source-map", // Helps with debugging TypeScript
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'), // Updated path
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'), // Updated path
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
