a83c2150227b73512062333af278c56c7c53b43f
Asem Asfandiarova <assemsim@gmail.com>
2025-05-08 19:33:54 +0600

    feat: connecct to Commercetools

---
+++ src/api/commerceToolsClient.ts
@@ -0,0 +1,28 @@
+import { AuthData as AUTH } from '@/api/token/auth-data.js';
+import { ClientBuilder, type AuthMiddlewareOptions, type HttpMiddlewareOptions } from '@commercetools/sdk-client-v2';
+
+const fetchApi = fetch;
+
+const projectKey: string = AUTH.projectKey;
+
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: AUTH.authUrl,
+  projectKey,
+  credentials: {
+    clientId: AUTH.clientId,
+    clientSecret: AUTH.clientSecret,
+  },
+  scopes: [AUTH.scopes],
+  fetch: fetchApi,
+};
+
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: AUTH.baseUrl,
+  fetch: fetchApi,
+};
+
+export const ctpClient = new ClientBuilder()
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withLoggerMiddleware()
+  .build();
---
+++ src/api/token/auth-data.ts
@@ -0,0 +1,15 @@
+export const AuthData = {
+  baseUrl: 'https://api.europe-west1.gcp.commercetools.com',
+  authUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+  projectKey: 'awesome-coffee-shop',
+  clientId: 'Z1U6D2Elsnu2FjrfXLF16Rz-',
+  clientSecret: 'U6CQIpoxl4M5ea2DPUV7pfU_GucUfKTP',
+  scopes:
+    'manage_project:awesome-coffee-shop view_audit_log:awesome-coffee-shop manage_api_clients:awesome-coffee-shop view_api_clients:awesome-coffee-shop',
+};
+
+// export type ACCESS_TOKEN = string;
+// export const TOKEN_TYPE: string = 'Bearer';
+// export type EXPIRES_IN = number;
+// export type SCOPE = string;
+//
