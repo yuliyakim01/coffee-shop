c16b140605380d5ec863879886c51461ea643372
Asem Asfandiarova <assemsim@gmail.com>
2025-05-14 15:32:21 +0600

    feat: create CustomerDraft from user input

---
+++ src/utils/customerUtils.ts
@@ -0,0 +1,17 @@
+import type { User } from '@/data/interfaces';
+import type { CustomerDraft } from '@commercetools/platform-sdk';
+
+export function createCustomerDraft(user: User, useAsDefaultAddress: boolean): CustomerDraft {
+  return {
+    email: user.email,
+    password: user.password,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    dateOfBirth: user.dateOfBirth,
+    ...(user.address && {
+      addresses: [user.address],
+      defaultShippingAddress: useAsDefaultAddress ? 0 : undefined,
+      defaultBillingAddress: useAsDefaultAddress ? 0 : undefined,
+    }),
+  };
+}
