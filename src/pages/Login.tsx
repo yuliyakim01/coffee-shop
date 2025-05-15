ecf5c30d1caaa484a2b5246cdf2cbdda32e9351f
Asem Asfandiarova <assemsim@gmail.com>
2025-05-15 12:50:43 +0600

    feat: add TogglePasswordVisibleButton

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { validateEmail, validatePassword } from '@/utils/validation';
-import eyeOnIcon from '../assets/eye.png';
-import eyeOffIcon from '../assets/eye-off.png';
 import BackButton from '@/components/Login-registration-components/BackButton';
 import Input from '@/components/Login-registration-components/Input';
+import TogglePasswordVisibleButton from '@/components/Login-registration-components/TogglePasswordVisibleButton';
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -63,17 +62,7 @@ const LoginPage: React.FC = () => {
               required
             />
 
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 -translate-y-1/2 w-[30px] h-[30px] bg-transparent p-0 cursor-pointer opacity-70 flex justify-center items-center"
-            >
-              <img
-                src={showPassword ? eyeOffIcon : eyeOnIcon}
-                alt={showPassword ? 'Hide password' : 'Show password'}
-                className="w-[20px] h-[20px] object-contain"
-              />
-            </button>
+            <TogglePasswordVisibleButton showPassword={showPassword} onToggle={() => setShowPassword(!showPassword)} />
 
             {passwordError && <p className="text-red-500 text-sm mt-1">{passwordError}</p>}
 
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -10,10 +10,9 @@ import {
   validatePostalCode,
   validateCountry,
 } from '@/utils/validation';
-import eyeOnIcon from '../assets/eye.png';
-import eyeOffIcon from '../assets/eye-off.png';
 import BackButton from '@/components/Login-registration-components/BackButton';
 import Input from '@/components/Login-registration-components/Input';
+import TogglePasswordVisibleButton from '@/components/Login-registration-components/TogglePasswordVisibleButton';
 
 const Register: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -197,17 +196,7 @@ const Register: React.FC = () => {
               }}
               required
             />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 -translate-y-1/2 w-[30px] h-[30px] bg-transparent p-0 cursor-pointer opacity-70 flex justify-center items-center"
-            >
-              <img
-                src={showPassword ? eyeOffIcon : eyeOnIcon}
-                alt={showPassword ? 'Hide password' : 'Show password'}
-                className="w-[20px] h-[20px] object-contain"
-              />
-            </button>
+            <TogglePasswordVisibleButton showPassword={showPassword} onToggle={() => setShowPassword(!showPassword)} />
             {passwordError && <p className="text-red-500 text-sm mt-1">{passwordError}</p>}
 
             <div className="flex justify-end w-full mt-1">
