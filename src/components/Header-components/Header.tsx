a374e1dd8704d63e038f40fe312a98652825849f
yuliyakim01 <yulia.kim@du.edu>
2025-05-20 10:27:50 +0500

    style: fixed button styling in modal, create hover effect, create animation for closing/opening of modal to be smoother

--- src/components/Header-components/Header.tsx
+++ src/components/Header-components/Header.tsx
@@ -25,48 +25,84 @@ function Header() {
         <nav>
           <ul className="flex flex-col items-center gap-8 text-white text-lg">
             <li>
-              <Link to={ROUTES.main} className="uppercase">
-                {LABELS.home}
+              <Link
+                to={ROUTES.main}
+                onClick={closeMenu}
+                className="group uppercase transition-transform duration-500 ease-in-out"
+              >
+                <span className="inline-block transition-transform duration-500 ease-in-out group-hover:scale-110">
+                  {LABELS.home}
+                </span>
               </Link>
             </li>
             <li>
-              <Link to={ROUTES.about} className="uppercase">
-                {LABELS.about}
+              <Link
+                to={ROUTES.about}
+                onClick={closeMenu}
+                className="group uppercase transition-transform duration-500 ease-in-out"
+              >
+                <span className="inline-block transition-transform duration-500 ease-in-out group-hover:scale-110">
+                  {LABELS.about}
+                </span>
               </Link>
             </li>
             <li>
-              <Link to={ROUTES.menu} className="uppercase">
-                {LABELS.menu}
+              <Link
+                to={ROUTES.menu}
+                onClick={closeMenu}
+                className="group uppercase transition-transform duration-500 ease-in-out"
+              >
+                <span className="inline-block transition-transform duration-500 ease-in-out group-hover:scale-110">
+                  {LABELS.menu}
+                </span>
               </Link>
             </li>
             <li>
-              <Link to={ROUTES.products} className="uppercase">
-                {LABELS.products}
+              <Link
+                to={ROUTES.products}
+                onClick={closeMenu}
+                className="group uppercase transition-transform duration-500 ease-in-out"
+              >
+                <span className="inline-block transition-transform duration-500 ease-in-out group-hover:scale-110">
+                  {LABELS.products}
+                </span>
               </Link>
             </li>
             <li>
-              <Link to={ROUTES.locations} className="uppercase">
-                {LABELS.locations}
+              <Link
+                to={ROUTES.locations}
+                onClick={closeMenu}
+                className="group uppercase transition-transform duration-500 ease-in-out"
+              >
+                <span className="inline-block transition-transform duration-500 ease-in-out group-hover:scale-110">
+                  {LABELS.locations}
+                </span>
               </Link>
             </li>
           </ul>
         </nav>
+
         <div className="mt-8">
           <Link
             to={ROUTES.login}
-            className="flex items-center justify-center gap-2 w-fit mx-auto uppercase bg-white text-black px-6 py-2 rounded-md text-lg font-medium hover:bg-gray-200 transition"
+            onClick={closeMenu}
+            className="group flex items-center justify-center gap-2 w-fit mx-auto uppercase bg-LightTaupe text-white px-6 py-2 rounded-md text-lg font-medium transition-transform duration-500 ease-in-out"
           >
             <img src={loginIcon} alt="Login icon" className="w-5 h-5" />
-            {LABELS.login}
+            <span className="transition-transform duration-500 ease-in-out group-hover:scale-110">{LABELS.login}</span>
           </Link>
         </div>
-        <div className="mt-8">
+
+        <div className="mt-4">
           <Link
             to={ROUTES.register}
-            className="flex items-center justify-center gap-2 w-fit mx-auto uppercase bg-white text-black px-6 py-2 rounded-md text-lg font-medium hover:bg-gray-200 transition"
+            onClick={closeMenu}
+            className="group flex items-center justify-center gap-2 w-fit mx-auto uppercase bg-LightTaupe text-white px-6 py-2 rounded-md text-lg font-medium transition-transform duration-500 ease-in-out"
           >
             <img src={registerIcon} alt="Register icon" className="w-5 h-5" />
-            {LABELS.register}
+            <span className="transition-transform duration-500 ease-in-out group-hover:scale-110">
+              {LABELS.register}
+            </span>
           </Link>
         </div>
       </div>
@@ -153,24 +189,24 @@ function Header() {
         </div>
       </div>
 
-      {/* Modal Overlay */}
-      {isOpen && (
-        <div className="fixed top-0 left-0 w-full h-screen z-10 flex">
-          {/* Overlay background */}
-          <div className="absolute inset-0 bg-black bg-opacity-50" onClick={closeMenu} />
+      {/* Modal Overlay - Always Mounted */}
+      <div
+        className={`fixed top-0 left-0 w-full h-screen z-10 flex transition-opacity duration-300 ${
+          isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+        }`}
+      >
+        {/* Overlay background */}
+        <div className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-500" onClick={closeMenu} />
 
-          {/* Modal content */}
-          <div className="relative z-20 bg-LightTaupe w-[70%] h-full flex flex-col justify-center items-center gap-8">
-            <img
-              src={close}
-              alt="Close-icon"
-              onClick={closeMenu}
-              className="w-8 absolute top-5 right-5 cursor-pointer"
-            />
-            {modal()}
-          </div>
+        {/* Modal content */}
+        <div
+          className={`relative z-20 bg-LightTaupe w-[70%] h-full flex flex-col justify-center items-center gap-8 
+      transform transition-transform duration-500 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        >
+          <img src={close} alt="Close-icon" onClick={closeMenu} className="w-8 absolute top-5 right-5 cursor-pointer" />
+          {modal()}
         </div>
-      )}
+      </div>
     </div>
   );
 }
