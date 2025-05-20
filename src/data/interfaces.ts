10b65cee598ae7819ff5866ab1083753538d88e8
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-05-20 12:55:06 +0400

    feat: add logutfunctionality

--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -178,3 +178,7 @@ export interface RegistrationFormItems {
   shippingPostalCode: string;
   shippingCountry: string;
 }
+export interface SessionUser {
+  customerId: string;
+  customerVersion: string;
+}
