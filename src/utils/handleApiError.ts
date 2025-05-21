792f16155b6d36d0173feb2f422bc13b320a1dd8
Asem Asfandiarova <assemsim@gmail.com>
2025-05-22 01:05:05 +0600

    fix: display of correct api error

--- src/utils/handleApiError.ts
+++ src/utils/handleApiError.ts
@@ -1,24 +1,30 @@
-import type { ApiError } from '@/data/interfaces';
+import type { CommercetoolsSdkError } from '@/data/interfaces';
 
 export default function handleApiError(error: unknown): string {
-  if (error instanceof Error && !('statusCode' in error)) {
-    return `Error: ${error.message}`;
+  if (isApiError(error)) {
+    const apiError = error.body;
+
+    if (apiError?.errors?.length) {
+      return `${apiError.errors[0].code}: ${apiError.errors[0].message}`;
+    }
+
+    if (apiError?.message) {
+      return `Error: ${apiError.message}`;
+    }
   }
 
-  if (isApiError(error)) {
-    const { message, errors } = error;
-
-    if (Array.isArray(errors) && errors.length > 0) {
-      return `${errors[0].code}: ${message}`;
-    }
-
-    if (message) {
-      return `Error: ${message}`;
-    }
+  if (error instanceof Error) {
+    return `Error: ${error.message}`;
   }
 
   return 'An unexpected error occurred. Please try again later.';
 }
-function isApiError(error: unknown): error is ApiError {
-  return typeof error === 'object' && error !== null && 'message' in error;
+
+function isApiError(error: unknown): error is CommercetoolsSdkError {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'body' in error &&
+    typeof (error as CommercetoolsSdkError).body?.message === 'string'
+  );
 }
