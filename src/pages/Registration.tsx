39eda1a1c7a515f15df2c8a43b2e2db5dd59fed9
Asem Asfandiarova <assemsim@gmail.com>
2025-05-24 22:31:01 +0600

    fix: bug 'logged-in user should not be able to navigate to Registration Page'

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import BackButton from '@/components/Login-registration-components/BackButton';
 import RegistrationFormComponent from '@/components/Login-registration-components/RegistrationFormComponent';
+import UserRedirect from '@/utils/useRedirect';
 
 const Register: React.FC = () => {
   return (
     <div className="min-h-screen bg-[#f0e6d7] px-4 py-12">
-      {/* Back Button aligned to the left at the top */}
+      <UserRedirect />
       <div className="w-full max-w-7xl mx-auto mb-6">
         <BackButton />
       </div>
 
-      {/* Registration Form centered below */}
       <div className="w-full max-w-3xl mx-auto bg-[#e6d7c2] rounded-2xl shadow-xl p-8">
         <h1 className="font-semibold text-4xl mb-8 text-center">Create an account</h1>
         <RegistrationFormComponent />
